b44152128165a6241c7c70524bfde44ec2f234dc
Nikhil-Jassal <200566092@student.georgianc.on.ca>
2025-08-05 09:15:14 -0400

    add repo movie

--- app/src/main/java/ca/georgian/assignment2/data/MovieRepository.kt
+++ app/src/main/java/ca/georgian/assignment2/data/MovieRepository.kt
@@ -1,3 +1,46 @@
 package ca.georgian.assignment2.data
 
-data class MovieRepository()
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class MovieRepository {
+    private val db = FirebaseFirestore.getInstance()
+    private val moviesCollection = db.collection("movies")
+
+    fun getAllMovies(): LiveData<List<Movie>>
+    {
+        val moviesLiveData = MutableLiveData<List<Movie>>()
+
+        moviesCollection.addSnapshotListener { snapshot, error ->
+            if (error != null) {
+                // Handle error
+                return@addSnapshotListener
+            }
+
+            val moviesList = mutableListOf<Movie>()
+            snapshot?.documents?.forEach { document ->
+                document.toObject(Movie::class.java)?.let { movie ->
+                    movie.id = document.id
+                    moviesList.add(movie)
+                }
+            }
+            moviesLiveData.value = moviesList
+        }
+
+        return moviesLiveData
+    }
+
+    fun addMovie(movie: Movie) {
+        moviesCollection.add(movie)
+    }
+
+    fun updateMovie(movie: Movie) {
+        movie.id?.let { id ->
+            moviesCollection.document(id).set(movie)
+        }
+    }
+
+    fun deleteMovie(movieId: String) {
+        moviesCollection.document(movieId).delete()
+    }
+}
